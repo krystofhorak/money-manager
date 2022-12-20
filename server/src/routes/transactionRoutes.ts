a0adecfc6463917fb8f02ef47abf779c8df27275
krystofhorak <krystof.horak@proton.me>
2022-12-20 21:04:05 +0100

    TS: Adding typings for the Transaction entity

--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 
+import type { Transaction } from '../../../typings/transactionTypes';
+
 const transactionRouter = Router();
 const currentDate: Date = new Date();
 
-const exampleTransactions: any[] = [
-  { transaction_id: 0, amount: 20.71, receiver: 'Walmart', date: currentDate.toJSON() },
-  { transaction_id: 1, amount: -6.30, receiver: 'Walmart', date: currentDate.toJSON() },
-  { transaction_id: 2, amount: 47.90, receiver: 'Walmart', date: currentDate.toJSON() },
-  { transaction_id: 3, amount: -62.35, receiver: 'Walmart', date: currentDate.toJSON() },
-  { transaction_id: 4, amount: 205.22, receiver: 'Walmart', date: currentDate.toJSON() },
+const exampleTransactions: Transaction[] = [
+  { transaction_id: 0, amount: 20.71, receiver: 'Walmart', sender: '', date: currentDate.toJSON() },
+  { transaction_id: 1, amount: -6.30, receiver: 'Walmart', sender: '', date: currentDate.toJSON() },
+  { transaction_id: 2, amount: 47.90, receiver: 'Walmart', sender: '', date: currentDate.toJSON() },
+  { transaction_id: 3, amount: -62.35, receiver: 'Walmart', sender: '', date: currentDate.toJSON() },
+  { transaction_id: 4, amount: 205.22, receiver: 'Walmart', sender: '', date: currentDate.toJSON() },
 ];
 
 transactionRouter.get('/', (_, res) => {
@@ -21,6 +23,7 @@ transactionRouter.post('/', (req, res) => {
     transaction_id: 0,
     amount,
     receiver,
+    sender: '',
     date,
   });
   res.json("Good!");
---
+++ typings/transactionTypes.ts
@@ -0,0 +1,7 @@
+export type Transaction = {
+  transaction_id: number;
+  amount: number;
+  receiver: string;
+  sender: string;
+  date: string;
+};
